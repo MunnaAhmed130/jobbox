7eb1fc7f8933cb82174c023e19aa0121d1e31b3e
Munna Ahmed <munnaahmed2025@gmail.com>
2023-08-10 13:50:59 +0600

    added constants file in assets

---
+++ src/assets/constants.js
@@ -0,0 +1,25 @@
+const businessCategory = [
+  "Automotive",
+  "Business Support & Supplies",
+  "Computers & Electronics",
+  "Construction & Contractors",
+  "Design Agency",
+  "Education",
+  "Entertainment",
+  "Food & Dining",
+  "Health & Medicine",
+  "Home & Garden",
+  "IT Farm",
+  "Legal & Financial",
+  "Manufacturing, Wholesale, Distribution",
+  "Merchants (Retail)",
+  "Miscellaneous",
+  "Personal Care & Services",
+  "Real Estate",
+  "Travel & Transportation",
+];
+
+const employeeRange = ["1 - 10", "11 - 50", "51 - 100", "Above 100"];
+const genderOption = ["male", "female", "other"];
+
+export { businessCategory, employeeRange, genderOption };
