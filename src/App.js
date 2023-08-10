e5991bdc55b363183cadf590de56505aa2957b4a
Munna Ahmed <munnaahmed2025@gmail.com>
2023-08-10 22:20:36 +0600

    refactor: minor changes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import routes from "./routes/routes";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import auth from "./firebase/firebase.config";
-import { useDispatch } from "react-redux";
-import { setUser, toggleLoading } from "./features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, setUser, toggleLoading } from "./features/auth/authSlice";
 import { Toaster } from "react-hot-toast";
 
 function App() {
   const dispatch = useDispatch();
 
-  // const user = useSelector((state) => state.auth.email);
-  // console.log(user);
+  const user = useSelector((state) => state.auth);
+  console.log(user);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         console.log(user);
-        dispatch(setUser(user.email));
+        // dispatch(setUser(user.email));
+        dispatch(getUser(user.email));
       } else {
         // console.log("Loading false");
         dispatch(toggleLoading());
