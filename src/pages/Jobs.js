063c05700980038bc96c47e7b6b4db9ddfbe9b93
Munna Ahmed <munnaahmed2025@gmail.com>
2023-08-13 15:28:28 +0600

    refactor the page for reuseabilty

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,20 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import JobCard from "../components/reusable/JobCard";
 import { useGetJobsQuery } from "../features/job/jobApi";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Jobs = () => {
-  const { data } = useGetJobsQuery();
-  console.log(data);
-  // const { position } = data.data || {};
+  const { data, isSuccess } = useGetJobsQuery();
+
+  const {
+    user: { _id },
+  } = useSelector((state) => state.auth);
+
+  const { pathname } = useLocation();
+
+  const postedJobs = data?.data?.filter((company) => company.companyId === _id);
+
+  // console.log(data);
   return (
     <div className="pt-14">
       <div className="bg-primary/10 p-5 rounded-2xl">
-        <h1 className="font-semibold text-xl">Find Jobs</h1>
+        <h1 className="font-semibold text-xl">
+          {pathname.includes("/job") ? "Find Jobs" : "Posted Jobs"}
+        </h1>
       </div>
       <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 mt-5">
-        {data?.data.map((jobData) => (
-          <JobCard jobData={jobData} key={jobData._id} />
-        ))}
+        {pathname.includes("/job") &&
+          isSuccess &&
+          data?.data?.map((jobData) => (
+            <JobCard jobData={jobData} key={jobData._id} />
+          ))}
+
+        {!pathname.includes("/job") &&
+          isSuccess &&
+          postedJobs.map((jobData) => (
+            <JobCard jobData={jobData} key={jobData._id} />
+          ))}
       </div>
     </div>
   );
